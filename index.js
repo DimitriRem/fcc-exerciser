530fb858f9b4ec57fee4948f4a50fd21b1d7529f
Dimitri Remoundos <dimitri.remoundos@gmail.com>
2024-03-18 08:57:55 +0000

    fixed form read

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const multer = require('multer')
 require('dotenv').config()
+const upload = multer();
 
 app.use(cors())
 app.use(express.static('public'))
@@ -9,8 +11,16 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+app.post("/api/users", upload.none(), (req, res) => {
+  let username = req.body.username;
+  
+  if (username === ""){
+    res.json({error: "please enter a user name"})}
 
 
+console.log(username);
+  res.json(username)
+});
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
